3f9242e3e36d68df1277818c0314ea2470d607f4
Hari <137824193+user-nameas@users.noreply.github.com>
2024-12-27 04:32:59 +0530

    product page

---
+++ Script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    document.getElementById("currentYear").textContent = new Date().getFullYear();
+    // Function to check if an element is in the viewport
+    function isElementInViewport(el) {
+        var rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+});
